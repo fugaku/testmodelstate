02d6024e03cf4e6a218d8a07eae82855aee734f4
daniel sagita <daniel.sagita@outlook.com>
2018-06-25 20:52:57 +0700

    add changes

--- TestModelState/Models/MyModel.cs
+++ TestModelState/Models/MyModel.cs
@@ -12,13 +12,13 @@ namespace TestModelState.Models
         [Required]
         [DisplayName("Start: ")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime? StartDate { get; set; }
 
         [Required]
         [DisplayName("End: ")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime? EndDate { get; set; }
     }
 }
--- TestModelState/TagHelpers/DatePickerTagHelper.cs
+++ TestModelState/TagHelpers/DatePickerTagHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using System;
@@ -7,39 +8,38 @@ namespace TestModelState.TagHelpers
 {
     public class DatePickerTagHelper : TagHelper
     {
-        IHtmlGenerator generator;
-        public DatePickerTagHelper(IHtmlGenerator generator)
+        public DatePickerTagHelper(IHtmlGenerator htmlGenerator)
+            //: base(htmlGenerator)
         {
-            this.generator = generator;
+            Generator = htmlGenerator;
         }
-
-        [HtmlAttributeNotBound]
-        [ViewContext]
-        public ViewContext ViewContext { get; set; }
 
         [HtmlAttributeName("for")]
         public ModelExpression For { get; set; }
 
-        [HtmlAttributeName("value")]
-        public string Value { get; set; }
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+        public IHtmlGenerator Generator { get; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+
             output.SuppressOutput();
-            var value = Value;
-            var format = "yyyy-MM-dd";
-            if (string.IsNullOrEmpty(value))
-            {
-                if (For != null && For.Model != null)
-                {
-                    var dt = DateTime.Parse(For.Model.ToString());
-                    value = dt.ToString(format);
-                }
-            }
-            var textbox = generator.GenerateTextBox(ViewContext, For.ModelExplorer, For.Name, value, format, null);
-            textbox.Attributes["value"] = value;
+            var format = "{0:dd/MM/yyyy}";
+            var textbox = Generator.GenerateTextBox(ViewContext, For.ModelExplorer, For.Name, For.ModelExplorer.Model, format, null);
             textbox.AddCssClass("form-control");
             output.Content.AppendHtml(textbox);
+
+            //base.For = this.For;
+            //base.Process(context, output);
+
+            //output.TagName = "input";
+            //output.TagMode = TagMode.StartTagOnly;
+
+            //TagBuilder input = new TagBuilder("input");
+            //input.AddCssClass("form-control");
+            //output.MergeAttributes(input);
         }
     }
 }
